7483312084dfb52977abcd3dd6186988b71f555f
sowmik-sec <sowmik.sec@gmail.com>
2023-04-13 15:32:06 +0600

    enum in typescript

--- dist/objectAndEnums.js
+++ dist/objectAndEnums.js
@@ -1,22 +1,43 @@
 "use strict";
-var player;
-player = {
-    name: "Ahsan",
-    age: 27,
-    country: "BD",
-    isMarried: false,
-    favoritePlace: ["Islampur", "Bashzani"],
-};
-console.log(player);
-player.age = (player === null || player === void 0 ? void 0 : player.age) + 5;
-console.log(player);
-player = {
-    name: "Habib",
-    //   age: 27,
-    country: "BD",
-    isMarried: false,
-    favoritePlace: ["Islampur", "Bashzani"],
-};
-console.log(player);
-player.favoritePlace && (player.favoritePlace[1] = "VoteHut");
-console.log(player);
+// Object
+// let player: {
+//   name: string;
+//   age?: number;
+//   country: string;
+//   isMarried: boolean;
+//   favoritePlace?: string[];
+// };
+// player = {
+//   name: "Ahsan",
+//   age: 27,
+//   country: "BD",
+//   isMarried: false,
+//   favoritePlace: ["Islampur", "Bashzani"],
+// };
+// console.log(player);
+// player.age = player?.age! + 5;
+// console.log(player);
+// player = {
+//   name: "Habib",
+//   //   age: 27,
+//   country: "BD",
+//   isMarried: false,
+//   favoritePlace: ["Islampur", "Bashzani"],
+// };
+// console.log(player);
+// player.favoritePlace && (player.favoritePlace[1] = "VoteHut");
+// console.log(player);
+// Enum
+var Friends;
+(function (Friends) {
+    Friends[Friends["Ahsan"] = 10] = "Ahsan";
+    Friends[Friends["Habib"] = 9] = "Habib";
+    Friends[Friends["Sowmik"] = 5] = "Sowmik";
+})(Friends || (Friends = {}));
+console.log(Friends[5]);
+var Configs;
+(function (Configs) {
+    Configs["API_Key"] = "api_key";
+    Configs["Domain_Name"] = "domain_name";
+})(Configs || (Configs = {}));
+console.log(Configs.Domain_Name);
--- src/objectAndEnums.ts
+++ src/objectAndEnums.ts
@@ -1,27 +1,43 @@
-let player: {
-  name: string;
-  age?: number;
-  country: string;
-  isMarried: boolean;
-  favoritePlace?: string[];
-};
-player = {
-  name: "Ahsan",
-  age: 27,
-  country: "BD",
-  isMarried: false,
-  favoritePlace: ["Islampur", "Bashzani"],
-};
-console.log(player);
-player.age = player?.age! + 5;
-console.log(player);
-player = {
-  name: "Habib",
-  //   age: 27,
-  country: "BD",
-  isMarried: false,
-  favoritePlace: ["Islampur", "Bashzani"],
-};
-console.log(player);
-player.favoritePlace && (player.favoritePlace[1] = "VoteHut");
-console.log(player);
+// Object
+// let player: {
+//   name: string;
+//   age?: number;
+//   country: string;
+//   isMarried: boolean;
+//   favoritePlace?: string[];
+// };
+// player = {
+//   name: "Ahsan",
+//   age: 27,
+//   country: "BD",
+//   isMarried: false,
+//   favoritePlace: ["Islampur", "Bashzani"],
+// };
+// console.log(player);
+// player.age = player?.age! + 5;
+// console.log(player);
+// player = {
+//   name: "Habib",
+//   //   age: 27,
+//   country: "BD",
+//   isMarried: false,
+//   favoritePlace: ["Islampur", "Bashzani"],
+// };
+// console.log(player);
+// player.favoritePlace && (player.favoritePlace[1] = "VoteHut");
+// console.log(player);
+
+// Enum
+
+enum Friends {
+  Ahsan = 10,
+  Habib = 9,
+  Sowmik = 5,
+}
+console.log(Friends[5]);
+
+enum Configs {
+  API_Key = "api_key",
+  Domain_Name = "domain_name",
+}
+console.log(Configs.Domain_Name);
