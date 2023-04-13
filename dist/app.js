a6f45ed01d876eb2dbb4213cdbe6ef4ea1e0e5b4
sowmik-sec <sowmik.sec@gmail.com>
2023-04-13 14:20:11 +0600

    initializing typescript

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var message = "Hello Typescript";
+var message2 = "Bye bye Javascript";
+console.log(message, message2, language);
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+const message = "Hello Typescript";
+const message2 = "Bye bye Javascript";
+
+console.log(message, message2, language);
