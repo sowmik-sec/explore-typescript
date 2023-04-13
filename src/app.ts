d8e2dfe6eb317ef0126a0d1a61df1febbce7f7e4
sowmik-sec <sowmik.sec@gmail.com>
2023-04-13 14:33:49 +0600

    primitive data types, uniontypes and how interred types in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
-const message = "Hello Typescript";
-const message2 = "Bye bye Javascript";
+// const message = "Hello Typescript";
+// const message2 = "Bye bye Javascript";
 
-console.log(message, message2, language);
+// console.log(message, message2, language);
+
+// let myName: string;
+
+// myName = "Sowmik";
+
+// let age: number | string;
+// age = "27";
+
+// let isMarried: boolean;
+// isMarried = false;
+
+let myName: "Ahsan" | "Habib" = "Ahsan";
+myName = "Habib";
