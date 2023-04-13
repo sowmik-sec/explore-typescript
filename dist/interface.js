a05de0bf6fdce23f58d56ba81692123ee2aad7b9
sowmik-sec <sowmik.sec@gmail.com>
2023-04-13 16:42:23 +0600

    explore interfaces and difference between type alias and interface

--- dist/interface.js
+++ dist/interface.js
@@ -1,37 +1,36 @@
 "use strict";
-// type MyName = string | number;
-// let myName: MyName = 542;
-// type MyName = "Ahsan" | "Habib";
-// let myName: MyName = "Habib";
-// console.log(myName);
-// type MyName = "Ahsan" | "Habib";
-// let myName: MyName = "Habib";
-// type arr = MyName[];
-// const arr: arr = ["Ahsan"];
-// type Players = [string, number, boolean];
-// let players: Players = ["Habib", 27, false];
-// type Add = (num1: number, num2: number, num3?: number) => number;
+// interface Person {
+//   name: string;
+//   email: string;
+// }
+// let person: Person = {
+//     name: "Ahsan",
+//     email: "a@h.com "
+// }
+// interface Add {
+//   (num1: number, num2: number, num3?: number): number;
+// }
 // const add: Add = (num1, num2, num3 = 0): number => {
 //   return num1 + num2 + num3;
 // };
-// type Address = {
+// interface Address {
 //   present: string;
 //   permanent: string;
-// };
-// type Favorite = {
+// }
+// interface Favorite {
 //   type: "food" | "player" | "star";
 //   value: string;
-// };
-// type Auth = {
+// }
+// interface Auth {
 //   isLoggedIn: boolean;
-// };
-// type Person = {
+// }
+// interface Person extends Auth {
 //   name: string;
-//   age: number;
+//   age?: number;
 //   phone: string | string[];
 //   address: Address;
-//   favorite: Favorite[];
-// } & Auth;
+//   favorite?: Favorite[];
+// }
 // const person: Person = {
 //   name: "Ahsan",
 //   age: 27,
--- src/typeAliases.ts
+++ src/typeAliases.ts
@@ -5,49 +5,54 @@
 // let myName: MyName = "Habib";
 // console.log(myName);
 
-type MyName = "Ahsan" | "Habib";
-let myName: MyName = "Habib";
-type arr = MyName[];
-const arr: arr = ["Ahsan"];
+// type MyName = "Ahsan" | "Habib";
+// let myName: MyName = "Habib";
+// type arr = MyName[];
+// const arr: arr = ["Ahsan"];
 
-type Players = [string, number, boolean];
+// type Players = [string, number, boolean];
 
-let players: Players = ["Habib", 27, false];
+// let players: Players = ["Habib", 27, false];
 
-type Add = (num1: number, num2: number, num3?: number) => number;
-const add: Add = (num1, num2, num3 = 0): number => {
-  return num1 + num2 + num3;
-};
+// type Add = (num1: number, num2: number, num3?: number) => number;
+// const add: Add = (num1, num2, num3 = 0): number => {
+//   return num1 + num2 + num3;
+// };
 
-type Address = {
-  present: string;
-  permanent: string;
-};
-type Favorite = {
-  type: "food" | "player" | "star";
-  value: string;
-};
+// type Address = {
+//   present: string;
+//   permanent: string;
+// };
+// type Favorite = {
+//   type: "food" | "player" | "star";
+//   value: string;
+// };
 
-type Person = {
-  name: string;
-  age: number;
-  phone: string | string[];
-  address: Address;
-  favorite: Favorite[];
-};
+// type Auth = {
+//   isLoggedIn: boolean;
+// };
 
-const person: Person = {
-  name: "Ahsan",
-  age: 27,
-  phone: "01333333333",
-  address: {
-    present: "Bhurungamari",
-    permanent: "Bhurungamari",
-  },
-  favorite: [
-    { type: "food", value: "Biriyani" },
-    { type: "star", value: "Melody" },
-  ],
-};
+// type Person = {
+//   name: string;
+//   age: number;
+//   phone: string | string[];
+//   address: Address;
+//   favorite: Favorite[];
+// } & Auth;
 
-console.log(person);
+// const person: Person = {
+//   name: "Ahsan",
+//   age: 27,
+//   phone: "01333333333",
+//   address: {
+//     present: "Bhurungamari",
+//     permanent: "Bhurungamari",
+//   },
+//   favorite: [
+//     { type: "food", value: "Biriyani" },
+//     { type: "star", value: "Melody" },
+//   ],
+//   isLoggedIn: false,
+// };
+
+// console.log(person);
