886f9f643c78bbb6ba536781684d501b194fc6d7
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 10:38:28 +0600

    return type in typescript

--- freecodecamp/myFunctions.ts
+++ freecodecamp/myFunctions.ts
@@ -1,10 +1,10 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
 }
 
 addTwo(5);
 
-function getUpper(val: string) {
+function getUpper(val: string): string {
   return val.toUpperCase();
 }
 
@@ -17,3 +17,27 @@ signUpUser("ahsan", "ah@san.com", false);
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 loginUser("ahsan", "ah@san.com");
+
+// function getValue(myVal: number) {
+//     if(myVal>5) {
+//         return true;
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+  return "hello";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
