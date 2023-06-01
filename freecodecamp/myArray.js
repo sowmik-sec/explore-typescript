f46d7a29c8e41bb1d55ef50c6429c48bd6d02e93
sowmik-sec <sowmik.sec@gmail.com>
2023-06-01 11:38:25 +0600

    array in typescript

---
+++ freecodecamp/myArray.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+superHeros.push("spiderman");
+heroPower.push(2);
+var allUsers = [];
+allUsers.push({ name: "habib", isActive: true });
+var MLModels = [
+    [255, 255, 255],
+    [247, 234, 231],
+];
---
+++ freecodecamp/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+type User = {
+  name: string;
+  isActive: boolean;
+};
+superHeros.push("spiderman");
+heroPower.push(2);
+
+const allUsers: User[] = [];
+
+allUsers.push({ name: "habib", isActive: true });
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [247, 234, 231],
+];
+
+export {};
